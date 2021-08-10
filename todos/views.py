e8566dfc57205cb4aadcafa572128da594a52e91
Wilton Souza <willsouzafilho@gmail.com>
2021-08-10 06:48:57 -0300

    Adding filters and ordering

--- todos/views.py
+++ todos/views.py
@@ -1,9 +1,11 @@
 from todos.serializers import TodoSerializer
 from todos.models import Todo
+from rest_framework import filters
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from authentication.jwt import JWTAuthentication
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class TodoAPIView(ListCreateAPIView):
@@ -11,6 +13,11 @@ class TodoAPIView(ListCreateAPIView):
     serializer_class = TodoSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = (IsAuthenticated,)
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields = ['id', 'title', 'desc', 'is_complete']
+    search_fields = ['id', 'title', 'desc', 'is_complete']
+    ordering_fields = ['id', 'title', 'desc', 'is_complete']
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
