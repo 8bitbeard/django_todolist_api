2a260bac4a91d9c2b492baf6c39b3b3ccf4eadac
Wilton Souza <willsouzafilho@gmail.com>
2021-08-10 06:30:13 -0300

    Reducing the Todo View to one class

--- todos/urls.py
+++ todos/urls.py
@@ -1,8 +1,10 @@
-from todos.views import CreateTodoAPIView, TodoListAPIView
+# from todos.views import CreateTodoAPIView, TodoListAPIView
+from todos.views import TodoAPIView
 from django.urls import path
 
 
 urlpatterns = [
-    path('create', CreateTodoAPIView.as_view(), name='create-todo'),
-    path('list', TodoListAPIView.as_view(), name='list-todo'),
+    path('', TodoAPIView.as_view(), name='todos'),
+    # path('create', CreateTodoAPIView.as_view(), name='create-todo'),
+    # path('list', TodoListAPIView.as_view(), name='list-todo'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,28 +1,40 @@
 from todos.serializers import TodoSerializer
 from todos.models import Todo
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from authentication.jwt import JWTAuthentication
 
 
-class CreateTodoAPIView(CreateAPIView):
+class TodoAPIView(ListCreateAPIView):
     serializer_class = TodoSerializer
-
     authentication_classes = [JWTAuthentication]
-
-    permission_classes=(IsAuthenticated,)
-    # permission_classes=[JWTAuthentication]
+    permission_classes = (IsAuthenticated,)
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
 
-
-class TodoListAPIView(ListAPIView):
-    serializer_class = TodoSerializer
-    authentication_classes = [JWTAuthentication]
-    permission_classes=(IsAuthenticated,)
-
-
     def get_queryset(self):
         return Todo.objects.filter(owner=self.request.user)
+
+
+# class CreateTodoAPIView(CreateAPIView):
+#     serializer_class = TodoSerializer
+
+#     authentication_classes = [JWTAuthentication]
+
+#     permission_classes=(IsAuthenticated,)
+#     # permission_classes=[JWTAuthentication]
+
+#     def perform_create(self, serializer):
+#         return serializer.save(owner=self.request.user)
+
+
+# class TodoListAPIView(ListAPIView):
+#     serializer_class = TodoSerializer
+#     authentication_classes = [JWTAuthentication]
+#     permission_classes=(IsAuthenticated,)
+
+
+#     def get_queryset(self):
+#         return Todo.objects.filter(owner=self.request.user)
